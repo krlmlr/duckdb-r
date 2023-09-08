b75c2fbd030151aa6d0ac9e16f9b98e48b99ef11
Kirill Müller <kirill@cynkra.com>
2023-09-08 11:37:36 +0200

    Use configure script

--- src/include/from-zip.mk
+++ src/include/from-zip.mk
@@ -1,7 +1,3 @@
 duckdb.tar: $(DUCKDB_R_PREBUILT_ARCHIVE)
-	pwd
-	echo $(DUCKDB_R_PREBUILT_ARCHIVE)
-	ls -l $(SOURCES) || true
-	tar -xmvf $(DUCKDB_R_PREBUILT_ARCHIVE)
 	ls -l $(SOURCES) || true
 	rm -f Makevars.duckdb
