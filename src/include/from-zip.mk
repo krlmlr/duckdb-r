d15d9e49659200833c4a3dc24294b655d068402f
Kirill Müller <kirill@cynkra.com>
2023-09-08 10:45:58 +0200

    REVERT ME: Extract twice, add debugging info

--- src/include/from-zip.mk
+++ src/include/from-zip.mk
@@ -1,3 +1,7 @@
 duckdb.tar: $(DUCKDB_R_PREBUILT_ARCHIVE)
+	pwd
+	echo $(DUCKDB_R_PREBUILT_ARCHIVE)
+	ls -l $(SOURCES) || true
+	tar -xmvf $(DUCKDB_R_PREBUILT_ARCHIVE)
 	ls -l $(SOURCES) || true
 	rm -f Makevars.duckdb
