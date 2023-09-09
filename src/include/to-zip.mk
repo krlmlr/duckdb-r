448eee797cf5b425eb9c65987b48eca4673460d5
Kirill Müller <kirill@cynkra.com>
2023-09-09 19:35:58 +0200

    Quotes

--- src/include/to-zip.mk
+++ src/include/to-zip.mk
@@ -1,3 +1,3 @@
 duckdb.tar: $(SOURCES)
-	if [ -n "$(DUCKDB_R_PREBUILT_ARCHIVE)" ]; then tar -cvf $(DUCKDB_R_PREBUILT_ARCHIVE) $(SOURCES); fi
+	if [ -n "$(DUCKDB_R_PREBUILT_ARCHIVE)" ]; then tar -cvf "$(DUCKDB_R_PREBUILT_ARCHIVE)"" $(SOURCES); fi
 	rm -f Makevars.duckdb
