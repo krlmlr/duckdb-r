879701800df4a123f2c53be490df6a2d1bae8d46
Kirill Müller <kirill@cynkra.com>
2023-09-10 20:17:42 +0200

    --force-local only on Windows

--- src/include/to-tar-win.mk
+++ src/include/to-tar-win.mk
@@ -2,5 +2,5 @@
 # or does not point to an existing file.
 # In the latter case, we create the file after the object files have been built.
 duckdb.tar: $(SOURCES)
-	if [ -n "$(DUCKDB_R_PREBUILT_ARCHIVE)" ]; then tar -cv -f "$(DUCKDB_R_PREBUILT_ARCHIVE)" $(SOURCES); fi
+	if [ -n "$(DUCKDB_R_PREBUILT_ARCHIVE)" ]; then tar -cv --force-local -f "$(DUCKDB_R_PREBUILT_ARCHIVE)" $(SOURCES); fi
 	rm -f Makevars.duckdb
