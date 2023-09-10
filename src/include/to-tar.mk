49da314d0fe9097ca8a029213f09e3dbedde23c0
Kirill Müller <kirill@cynkra.com>
2023-09-10 22:55:32 +0200

    Revert "Logging, again"
    This reverts commit bd329fb32a03ad20febd02069322ef1faedd93ac.

--- src/include/to-tar.mk
+++ src/include/to-tar.mk
@@ -2,6 +2,5 @@
 # or does not point to an existing file.
 # In the latter case, we create the file after the object files have been built.
 duckdb.tar: $(SOURCES)
-	echo "Creating .tar file: $(DUCKDB_R_PREBUILT_ARCHIVE)"
 	if [ -n "$(DUCKDB_R_PREBUILT_ARCHIVE)" ]; then tar -cv -f "$(DUCKDB_R_PREBUILT_ARCHIVE)" $(SOURCES); fi
 	rm -f Makevars.duckdb
