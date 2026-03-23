beef783eff6a5b6e5bbba853415de7c1f4406cee
krlmlr <krlmlr@users.noreply.github.com>
2026-03-23 00:33:52 +0000

    vendor: Update vendored sources to duckdb/duckdb@03ea0f215462def9c8c90a095b43e861537bf07f
    Date: 2026-03-09 09:14:32 +0100
    
    Internal duckdb/duckdb#7568: AsOf Simple Joins  (duckdb/duckdb#21230)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev8481"
+#define DUCKDB_PATCH_VERSION "0-dev8491"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev8481"
+#define DUCKDB_VERSION "v1.5.0-dev8491"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "64d587331c"
+#define DUCKDB_SOURCE_ID "03ea0f2154"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
