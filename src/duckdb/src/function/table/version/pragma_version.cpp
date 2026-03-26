92ae5a83d93850d2434b6a2ad0d07fb1e1e68e54
krlmlr <krlmlr@users.noreply.github.com>
2026-03-26 08:14:55 +0000

    vendor: Update vendored sources to duckdb/duckdb@7fe6d721edede464c6aac41d9ca8547d8016ee9a
    Date: 2026-03-24 13:10:00 +0100
    
    Fix wasm build: use __EMSCRIPTEN__ instead of deprecated EMSCRIPTEN (duckdb/duckdb#21576)
    Fix cardinality estimation for uncommitted rows (duckdb/duckdb#21573)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2049"
+#define DUCKDB_PATCH_VERSION "0-dev2053"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev2049"
+#define DUCKDB_VERSION "v1.6.0-dev2053"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "3c9066ad59"
+#define DUCKDB_SOURCE_ID "7fe6d721ed"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
