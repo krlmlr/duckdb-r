2f6fdd67958bb6b26e8e27422106c5bed3466a45
krlmlr <krlmlr@users.noreply.github.com>
2026-03-26 08:15:12 +0000

    vendor: Update vendored sources to duckdb/duckdb@44f79e1161fbf170d019beb553773ebc49731144
    Date: 2026-03-25 15:25:35 +0100
    
    Vector: make `Flatten` work for `const` vectors, and remove implicit sort-of-copy constructor (duckdb/duckdb#21612)
    Run windows amd64 CI job on namespace runner (duckdb/duckdb#21557)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2097"
+#define DUCKDB_PATCH_VERSION "0-dev2119"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev2097"
+#define DUCKDB_VERSION "v1.6.0-dev2119"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "7d8ff05a29"
+#define DUCKDB_SOURCE_ID "44f79e1161"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
