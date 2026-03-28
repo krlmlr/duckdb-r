f6709cd01e96b21ff12a4435d5163c71eeec5507
krlmlr <krlmlr@users.noreply.github.com>
2026-03-28 22:04:46 +0000

    vendor: Update vendored sources to duckdb/duckdb@3c9066ad59b6bc8540d76ed762b42a744b179551
    Date: 2026-03-23 20:42:16 +0100
    
    Re-organize vectors: move various vector methods to different files (https://redirect.github.com/duckdb/duckdb/pull/21563)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2046"
+#define DUCKDB_PATCH_VERSION "0-dev2049"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev2046"
+#define DUCKDB_VERSION "v1.6.0-dev2049"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "3ac07aebe5"
+#define DUCKDB_SOURCE_ID "3c9066ad59"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
