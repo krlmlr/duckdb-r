65d19a9fc05bf1b9f05ef2fc06965d3ecac6fca3
krlmlr <krlmlr@users.noreply.github.com>
2026-03-29 06:22:34 +0000

    vendor: Update vendored sources to duckdb/duckdb@c38024fd15ecc2a28a37647db81807a311423c1d
    Date: 2026-03-26 10:14:00 +0100
    
    Implement block-aligned read and cache for external file cache (https://redirect.github.com/duckdb/duckdb/pull/21395)
    [autocomplete] Use AutoCompleteCatalogProvider abstraction (https://redirect.github.com/duckdb/duckdb/pull/21621)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2152"
+#define DUCKDB_PATCH_VERSION "0-dev2232"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev2152"
+#define DUCKDB_VERSION "v1.6.0-dev2232"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "8d114b711c"
+#define DUCKDB_SOURCE_ID "c38024fd15"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
