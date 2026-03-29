c6924b70484ae60e15750af939c60fc17b10d16e
krlmlr <krlmlr@users.noreply.github.com>
2026-03-29 06:22:58 +0000

    vendor: Update vendored sources to duckdb/duckdb@9575d227bc216d628dcb888336e3c1f811e7b8c7
    Date: 2026-03-27 19:29:13 +0100
    
    Fix race condition: successful COMMIT incorrectly reported as interrupted (https://redirect.github.com/duckdb/duckdb/pull/21616)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2335"
+#define DUCKDB_PATCH_VERSION "0-dev2337"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev2335"
+#define DUCKDB_VERSION "v1.6.0-dev2337"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ea2c86420a"
+#define DUCKDB_SOURCE_ID "9575d227bc"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
