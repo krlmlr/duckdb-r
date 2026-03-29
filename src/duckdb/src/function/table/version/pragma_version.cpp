27fc05a1f11e324824da4f7eb97ecf0d21c654b8
krlmlr <krlmlr@users.noreply.github.com>
2026-03-29 06:22:56 +0000

    vendor: Update vendored sources to duckdb/duckdb@ea2c86420a8adb9a8dc1aeac3ddad24fb5854d90
    Date: 2026-03-27 19:23:14 +0100
    
    Fix a few class construction UB (https://redirect.github.com/duckdb/duckdb/pull/21670)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2333"
+#define DUCKDB_PATCH_VERSION "0-dev2335"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev2333"
+#define DUCKDB_VERSION "v1.6.0-dev2335"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "bafce912d6"
+#define DUCKDB_SOURCE_ID "ea2c86420a"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
