889015c6ad06784351316ce5efbd87eb40338deb
krlmlr <krlmlr@users.noreply.github.com>
2026-04-01 09:23:43 +0000

    vendor: Update vendored sources to duckdb/duckdb@b149638b584179a4b15bbc9eecbbbc51d51c330d
    Date: 2026-04-01 10:51:13 +0200
    
    FIX https://redirect.github.com/duckdb/duckdb/pull/21506 UNNEST ARRAY type functionality (https://redirect.github.com/duckdb/duckdb/pull/21672)
    Improve some CI jobs (https://redirect.github.com/duckdb/duckdb/pull/21736)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2686"
+#define DUCKDB_PATCH_VERSION "0-dev2698"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev2686"
+#define DUCKDB_VERSION "v1.6.0-dev2698"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ebc4a240c7"
+#define DUCKDB_SOURCE_ID "b149638b58"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
