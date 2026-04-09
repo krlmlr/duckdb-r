7f6eb6721ed73c12c6849c1a3a70e7cc57ef308d
krlmlr <krlmlr@users.noreply.github.com>
2026-04-09 19:25:30 +0000

    vendor: Update vendored sources to duckdb/duckdb@04478f84cc8872ac233714aaaae36f268faa15fd
    Date: 2026-04-09 18:19:54 +0000
    
    Remove constructor that allows for creation of uninitialized vectors, and add an enum instead of using a bool for initialization (https://redirect.github.com/duckdb/duckdb/pull/21995)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3271"
+#define DUCKDB_PATCH_VERSION "0-dev3277"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev3271"
+#define DUCKDB_VERSION "v1.6.0-dev3277"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "111e91b87f"
+#define DUCKDB_SOURCE_ID "04478f84cc"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
