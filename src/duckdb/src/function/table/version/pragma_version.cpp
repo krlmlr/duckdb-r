fc90eeba0e24e2309317e4460ae299d2dc534297
krlmlr <krlmlr@users.noreply.github.com>
2026-04-15 18:25:12 +0000

    vendor: Update vendored sources to duckdb/duckdb@e54cc78e319c33594ad4bb8db8f464f78d99c636
    Date: 2026-04-15 17:29:25 +0000
    
    Fix bignum constant casting (https://redirect.github.com/duckdb/duckdb/pull/22084)
    Write merged vcpkg.json to build dir (build/reldebug/vcpkg.json), always write vcpkg.json, and clean-up Makefile by unifying the various builds (https://redirect.github.com/duckdb/duckdb/pull/22090)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3514"
+#define DUCKDB_PATCH_VERSION "0-dev3522"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev3514"
+#define DUCKDB_VERSION "v1.6.0-dev3522"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f964d38dbc"
+#define DUCKDB_SOURCE_ID "e54cc78e31"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
