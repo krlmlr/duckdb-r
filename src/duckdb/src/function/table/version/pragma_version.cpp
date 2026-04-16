80769e896881be9a6e16729c8ba7e3988f18ae63
krlmlr <krlmlr@users.noreply.github.com>
2026-04-16 15:33:25 +0000

    vendor: Update vendored sources to duckdb/duckdb@471de9f0e0e157ae672e56710e8c43b132a5ddc4
    Date: 2026-04-16 13:11:27 +0000
    
    PEG parser change `optional_ptr` to references (https://redirect.github.com/duckdb/duckdb/pull/22111)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3772"
+#define DUCKDB_PATCH_VERSION "0-dev3783"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev3772"
+#define DUCKDB_VERSION "v1.6.0-dev3783"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4d0108e107"
+#define DUCKDB_SOURCE_ID "471de9f0e0"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
