230759015d6b2b649f62de299970bfa17ab48499
krlmlr <krlmlr@users.noreply.github.com>
2026-04-20 07:37:05 +0000

    vendor: Update vendored sources to duckdb/duckdb@bf5740c1f7955233dff926cb29cdb2d9684ac375
    Date: 2026-04-20 07:28:56 +0000
    
    Fix TRY_CAST debug assertion failure for MAP (https://redirect.github.com/duckdb/duckdb/pull/22152)
    PEG script verify grammar (https://redirect.github.com/duckdb/duckdb/pull/22140)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3879"
+#define DUCKDB_PATCH_VERSION "0-dev3891"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev3879"
+#define DUCKDB_VERSION "v1.6.0-dev3891"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ccbb21804f"
+#define DUCKDB_SOURCE_ID "bf5740c1f7"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
