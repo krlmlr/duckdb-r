bbb986dab0810d833369a953c677389447e14fe9
krlmlr <krlmlr@users.noreply.github.com>
2026-04-22 08:35:25 +0000

    vendor: Update vendored sources to duckdb/duckdb@19925dce89f9b48e206264593c8a1920eaeed673
    Date: 2026-04-22 07:57:55 +0000
    
    GCC 12 could not convert unique_ptr result (https://redirect.github.com/duckdb/duckdb/pull/22210)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev4189"
+#define DUCKDB_PATCH_VERSION "0-dev4191"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev4189"
+#define DUCKDB_VERSION "v1.6.0-dev4191"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "56404c3203"
+#define DUCKDB_SOURCE_ID "19925dce89"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
