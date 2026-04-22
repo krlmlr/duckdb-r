20e7b70354596eb6095a52cdb2a70c4b7e39c0d8
krlmlr <krlmlr@users.noreply.github.com>
2026-04-22 07:34:15 +0000

    vendor: Update vendored sources to duckdb/duckdb@56404c3203ddd794c6d0852320d8cf3bcbd886f1
    Date: 2026-04-22 07:08:20 +0000
    
    Vector Rework: Add append methods to `Vector`, and remove `source_offset` from ListAppend (https://redirect.github.com/duckdb/duckdb/pull/22205)
    fix failures summary in test runner (https://redirect.github.com/duckdb/duckdb/pull/22196)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev4183"
+#define DUCKDB_PATCH_VERSION "0-dev4189"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev4183"
+#define DUCKDB_VERSION "v1.6.0-dev4189"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "8169730ac0"
+#define DUCKDB_SOURCE_ID "56404c3203"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
