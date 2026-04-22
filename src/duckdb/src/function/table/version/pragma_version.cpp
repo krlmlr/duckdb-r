a27f0f95abcbba57e419cc8b6998d1002022e381
krlmlr <krlmlr@users.noreply.github.com>
2026-04-22 12:21:39 +0000

    vendor: Update vendored sources to duckdb/duckdb@fb019db70c447eb2013ce94e0da400c66b580ad3
    Date: 2026-04-22 11:53:32 +0000
    
    Unify struct filter with expression filter (https://redirect.github.com/duckdb/duckdb/pull/22207)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev4211"
+#define DUCKDB_PATCH_VERSION "0-dev4218"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev4211"
+#define DUCKDB_VERSION "v1.6.0-dev4218"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "27a2f193b8"
+#define DUCKDB_SOURCE_ID "fb019db70c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
