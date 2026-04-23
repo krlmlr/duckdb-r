b8a383c91d4d1637edbb36ba2145a3b866d4b8d1
krlmlr <krlmlr@users.noreply.github.com>
2026-04-23 08:36:17 +0000

    vendor: Update vendored sources to duckdb/duckdb@7dc6e32aaf84e1c50a4f4f5071e6022c534f1155
    Date: 2026-04-23 08:18:30 +0000
    
    Internal https://redirect.github.com/duckdb/duckdb/pull/7385: ICU Unity Builds (https://redirect.github.com/duckdb/duckdb/pull/22175)

--- src/duckdb/ub_src_storage.cpp
+++ src/duckdb/ub_src_storage.cpp
@@ -1,14 +1,12 @@
 #include "src/storage/arena_allocator.cpp"
 
-#include "src/storage/buffer_manager.cpp"
-
-#include "src/storage/checkpoint_manager.cpp"
-
-#include "src/storage/temporary_memory_manager.cpp"
-
 #include "src/storage/block.cpp"
 
 #include "src/storage/block_allocator.cpp"
+
+#include "src/storage/buffer_manager.cpp"
+
+#include "src/storage/checkpoint_manager.cpp"
 
 #include "src/storage/data_pointer.cpp"
 
@@ -20,16 +18,6 @@
 
 #include "src/storage/magic_bytes.cpp"
 
-#include "src/storage/storage_manager.cpp"
-
-#include "src/storage/standard_buffer_manager.cpp"
-
-#include "src/storage/temporary_file_manager.cpp"
-
-#include "src/storage/table_index_list.cpp"
-
-#include "src/storage/write_ahead_log.cpp"
-
 #include "src/storage/open_file_storage_extension.cpp"
 
 #include "src/storage/optimistic_data_writer.cpp"
@@ -38,11 +26,23 @@
 
 #include "src/storage/single_file_block_manager.cpp"
 
+#include "src/storage/standard_buffer_manager.cpp"
+
 #include "src/storage/storage_index.cpp"
 
 #include "src/storage/storage_info.cpp"
 
 #include "src/storage/storage_lock.cpp"
 
+#include "src/storage/storage_manager.cpp"
+
+#include "src/storage/table_index_list.cpp"
+
+#include "src/storage/temporary_file_manager.cpp"
+
+#include "src/storage/temporary_memory_manager.cpp"
+
 #include "src/storage/wal_replay.cpp"
+
+#include "src/storage/write_ahead_log.cpp"
 
