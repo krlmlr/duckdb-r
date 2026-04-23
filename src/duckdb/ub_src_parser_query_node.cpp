8e2c995617038e30c099406dc44e75088e60daad
krlmlr <krlmlr@users.noreply.github.com>
2026-04-23 08:36:17 +0000

    vendor: Update vendored sources to duckdb/duckdb@7dc6e32aaf84e1c50a4f4f5071e6022c534f1155
    Date: 2026-04-23 08:18:30 +0000
    
    Internal https://redirect.github.com/duckdb/duckdb/pull/7385: ICU Unity Builds (https://redirect.github.com/duckdb/duckdb/pull/22175)

--- src/duckdb/ub_src_parser_query_node.cpp
+++ src/duckdb/ub_src_parser_query_node.cpp
@@ -1,6 +1,10 @@
-#include "src/parser/query_node/recursive_cte_node.cpp"
-
 #include "src/parser/query_node/cte_node.cpp"
+
+#include "src/parser/query_node/delete_query_node.cpp"
+
+#include "src/parser/query_node/insert_query_node.cpp"
+
+#include "src/parser/query_node/recursive_cte_node.cpp"
 
 #include "src/parser/query_node/select_node.cpp"
 
@@ -9,8 +13,4 @@
 #include "src/parser/query_node/statement_node.cpp"
 
 #include "src/parser/query_node/update_query_node.cpp"
-
-#include "src/parser/query_node/delete_query_node.cpp"
-
-#include "src/parser/query_node/insert_query_node.cpp"
 
