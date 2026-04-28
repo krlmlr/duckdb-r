936793952f8d287bcb4383b131b59b2ae29df2e0
krlmlr <krlmlr@users.noreply.github.com>
2026-04-28 11:34:23 +0000

    vendor: Update vendored sources to duckdb/duckdb@77ee7d296d22bbbb0882c43d2b64d7a658276fa7
    Date: 2026-04-28 10:08:03 +0000
    
    CLI: PostBind hook to determine if _ might be used in query, if not, reclaim memory early (https://redirect.github.com/duckdb/duckdb/pull/22290)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev4783"
+#define DUCKDB_PATCH_VERSION "0-dev4787"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev4783"
+#define DUCKDB_VERSION "v1.6.0-dev4787"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "16fab311be"
+#define DUCKDB_SOURCE_ID "77ee7d296d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
