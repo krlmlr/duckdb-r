e8000fe74505f7bb87eb359068ca560b32eed7ed
krlmlr <krlmlr@users.noreply.github.com>
2026-04-29 12:32:04 +0000

    vendor: Update vendored sources to duckdb/duckdb@20cf1029f0cce589444bfa60ab545634762d4db8
    Date: 2026-04-29 12:20:29 +0000
    
    Deprecate `PRAGMA enable_verification` and friends, move `verify_serializer` to separate option and verification run (https://redirect.github.com/duckdb/duckdb/pull/22361)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev4871"
+#define DUCKDB_PATCH_VERSION "0-dev4878"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev4871"
+#define DUCKDB_VERSION "v1.6.0-dev4878"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "aef61d6db6"
+#define DUCKDB_SOURCE_ID "20cf1029f0"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
