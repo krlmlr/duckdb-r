7d77b34bd9bad95d1a052e33fa606f8435ec10b9
krlmlr <krlmlr@users.noreply.github.com>
2026-04-29 12:32:02 +0000

    vendor: Update vendored sources to duckdb/duckdb@aef61d6db6ad8f57f51bb923bbbf4848929cdc0c
    Date: 2026-04-29 11:58:34 +0000
    
    Flat struct_map list to get the correct size (https://redirect.github.com/duckdb/duckdb/pull/22346)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev4865"
+#define DUCKDB_PATCH_VERSION "0-dev4871"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev4865"
+#define DUCKDB_VERSION "v1.6.0-dev4871"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a41ee38175"
+#define DUCKDB_SOURCE_ID "aef61d6db6"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
