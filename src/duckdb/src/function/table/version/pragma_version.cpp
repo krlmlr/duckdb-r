03d3dfc2cd2bc19554a55cc637c9f239e1730800
krlmlr <krlmlr@users.noreply.github.com>
2026-04-29 20:25:36 +0000

    vendor: Update vendored sources to duckdb/duckdb@ae64b51a089a1c2fcab0d8f045e101b87980f204
    Date: 2026-04-29 20:17:35 +0000
    
    Deprecate `KeywordHelper::Write[Optionally]Quoted` - and improve support for `SQLString` and `SQLIdentifier` instead (https://redirect.github.com/duckdb/duckdb/pull/22374)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev4973"
+#define DUCKDB_PATCH_VERSION "0-dev4979"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev4973"
+#define DUCKDB_VERSION "v1.6.0-dev4979"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "793b055a4f"
+#define DUCKDB_SOURCE_ID "ae64b51a08"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
