c930fd9c6aa1ef1e4872cf85ec659dde7f1308f7
krlmlr <krlmlr@users.noreply.github.com>
2026-04-30 12:32:24 +0000

    vendor: Update vendored sources to duckdb/duckdb@5cc4ee49c2e272d1e5d2bf373625b05f7ca0dbd1
    Date: 2026-04-30 12:30:05 +0000
    
    Fix vector size issue in extensions  (https://redirect.github.com/duckdb/duckdb/pull/22391)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev5039"
+#define DUCKDB_PATCH_VERSION "0-dev5042"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev5039"
+#define DUCKDB_VERSION "v1.6.0-dev5042"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a28d95d5b4"
+#define DUCKDB_SOURCE_ID "5cc4ee49c2"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
