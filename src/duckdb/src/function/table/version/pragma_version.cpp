e5eb299a0b22cb6c42ddbe2628f4f9d8c09fb68c
krlmlr <krlmlr@users.noreply.github.com>
2026-05-01 15:22:20 +0000

    vendor: Update vendored sources to duckdb/duckdb@2d3868df17bab6a95ed4eaff97adf81d4e39b9eb
    Date: 2026-05-01 14:24:18 +0000
    
    Add support for Vector::Values<VectorListType<...>> allowing iteration over lists with a fixed (known) schema (https://redirect.github.com/duckdb/duckdb/pull/22422)
    Ignore queue branch for push and merge tidy check jobs (https://redirect.github.com/duckdb/duckdb/pull/22424)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev5120"
+#define DUCKDB_PATCH_VERSION "0-dev5131"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev5120"
+#define DUCKDB_VERSION "v1.6.0-dev5131"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "7a6be3c62b"
+#define DUCKDB_SOURCE_ID "2d3868df17"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
