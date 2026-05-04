807fc0b8a5487a00ea01b6eab665a1cb42ef3f2c
krlmlr <krlmlr@users.noreply.github.com>
2026-05-04 14:44:58 +0000

    vendor: Update vendored sources to duckdb/duckdb@4d5a2246bb69cf892b9d1e01bf3b98c09eb86e99
    Date: 2026-05-04 13:21:00 +0000
    
    Make external file cache size tunable (https://redirect.github.com/duckdb/duckdb/pull/21700)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev5281"
+#define DUCKDB_PATCH_VERSION "0-dev5312"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev5281"
+#define DUCKDB_VERSION "v1.6.0-dev5312"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "61edc90ffa"
+#define DUCKDB_SOURCE_ID "4d5a2246bb"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
