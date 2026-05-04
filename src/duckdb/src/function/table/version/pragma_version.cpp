2744c13097a995615f5ada707a486d0729cd97da
krlmlr <krlmlr@users.noreply.github.com>
2026-05-04 07:01:40 +0000

    vendor: Update vendored sources to duckdb/duckdb@24c543706576db08a78bd59490d6164b5bef392d
    Date: 2026-05-04 06:44:54 +0000
    
    Improved `FILE_SIZE_BYTES` / `ROW_GROUPS_PER_FILE` (https://redirect.github.com/duckdb/duckdb/pull/22423)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev5181"
+#define DUCKDB_PATCH_VERSION "0-dev5201"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev5181"
+#define DUCKDB_VERSION "v1.6.0-dev5201"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "6be1980b1e"
+#define DUCKDB_SOURCE_ID "24c5437065"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
