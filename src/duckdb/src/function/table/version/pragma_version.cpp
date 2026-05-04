f1e4a2534c383a78d367f623b86cf2abe3b56a53
krlmlr <krlmlr@users.noreply.github.com>
2026-05-04 08:50:10 +0000

    vendor: Update vendored sources to duckdb/duckdb@1c0f8ace6ddac1ecbed945ecbfb68e4ce68de256
    Date: 2026-05-04 06:53:06 +0000
    
    Make multi-file scan's filter pushdown skip files instead of opening them (https://redirect.github.com/duckdb/duckdb/pull/22263)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev5201"
+#define DUCKDB_PATCH_VERSION "0-dev5206"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev5201"
+#define DUCKDB_VERSION "v1.6.0-dev5206"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "24c5437065"
+#define DUCKDB_SOURCE_ID "1c0f8ace6d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
