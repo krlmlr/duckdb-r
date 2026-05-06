3e02a621a5dd7fd87657ed37eefc90a052b710f0
krlmlr <krlmlr@users.noreply.github.com>
2026-05-06 14:54:58 +0000

    vendor: Update vendored sources to duckdb/duckdb@3bea18e9978c05040ec7457b71c003e9b570e7a6
    Date: 2026-05-06 13:09:15 +0000
    
    Enhance interval type parsing precision and PostgreSQL compatibility. (https://redirect.github.com/duckdb/duckdb/pull/22130)
    Reduce compile log output and remove old test runner (https://redirect.github.com/duckdb/duckdb/pull/22491)
    bump duckdb-sqlsmith (https://redirect.github.com/duckdb/duckdb/pull/22494)
    Fix vsize = 2 issue in JSON create (https://redirect.github.com/duckdb/duckdb/pull/22492)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev5451"
+#define DUCKDB_PATCH_VERSION "0-dev5465"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev5451"
+#define DUCKDB_VERSION "v1.6.0-dev5465"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "63d32ec47d"
+#define DUCKDB_SOURCE_ID "3bea18e997"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
