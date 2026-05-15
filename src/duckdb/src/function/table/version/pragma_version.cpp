9c6421393a7674dd5e8913356e07d0963f3ce1e2
krlmlr <krlmlr@users.noreply.github.com>
2026-05-15 09:03:06 +0000

    vendor: Update vendored sources to duckdb/duckdb@6dd33fcd90f633872340322a9d2401ea826eab56
    Date: 2026-05-15 07:42:25 +0000
    
    Parquet ApplyPendingSkips really skip decode (https://redirect.github.com/duckdb/duckdb/pull/22664)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev5934"
+#define DUCKDB_PATCH_VERSION "0-dev5937"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev5934"
+#define DUCKDB_VERSION "v1.6.0-dev5937"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "884cdb8ac0"
+#define DUCKDB_SOURCE_ID "6dd33fcd90"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
