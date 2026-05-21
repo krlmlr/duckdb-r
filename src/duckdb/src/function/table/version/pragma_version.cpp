1ab8d061d3c77e73ff1424fa131e32ac6429fe02
krlmlr <krlmlr@users.noreply.github.com>
2026-05-21 15:04:54 +0000

    vendor: Update vendored sources to duckdb/duckdb@2f774b74754fe44c895ae80c2254b79eac0f3f68
    Date: 2026-05-21 13:32:39 +0000
    
    Support long paths on Windows (https://redirect.github.com/duckdb/duckdb/pull/20983)
    Run more core extensions with sanitizers in CI (https://redirect.github.com/duckdb/duckdb/pull/22689)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6697"
+#define DUCKDB_PATCH_VERSION "0-dev6703"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev6697"
+#define DUCKDB_VERSION "v1.6.0-dev6703"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "05bc1b711c"
+#define DUCKDB_SOURCE_ID "2f774b7475"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
