22d84c2b449556572635ab8f4608e10ac704e63e
krlmlr <krlmlr@users.noreply.github.com>
2026-05-26 14:08:24 +0000

    vendor: Update vendored sources to duckdb/duckdb@7c55bc1be322ff492895b1b5de1d9ccecacc0744
    Date: 2026-05-26 11:54:46 +0000
    
    Add support for large integer values for CSV (https://redirect.github.com/duckdb/duckdb/pull/22802)
    Re-enable all explicitly disabled tests on musl (https://redirect.github.com/duckdb/duckdb/pull/22877)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6922"
+#define DUCKDB_PATCH_VERSION "0-dev6937"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev6922"
+#define DUCKDB_VERSION "v1.6.0-dev6937"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "28a1d85be6"
+#define DUCKDB_SOURCE_ID "7c55bc1be3"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
