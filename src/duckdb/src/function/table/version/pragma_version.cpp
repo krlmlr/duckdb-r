0092229331b8e0f3d3412a32984a9ce18c07b7a2
krlmlr <krlmlr@users.noreply.github.com>
2026-05-28 09:13:36 +0000

    vendor: Update vendored sources to duckdb/duckdb@1ce496bb2e3e20ae1299e0d75f1d4a3b33cc3332
    Date: 2026-05-28 07:07:16 +0000
    
    Internal https://redirect.github.com/duckdb/duckdb/pull/9307: Window Batching (https://redirect.github.com/duckdb/duckdb/pull/22794)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7061"
+#define DUCKDB_PATCH_VERSION "0-dev7074"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev7061"
+#define DUCKDB_VERSION "v1.6.0-dev7074"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "811109f444"
+#define DUCKDB_SOURCE_ID "1ce496bb2e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
