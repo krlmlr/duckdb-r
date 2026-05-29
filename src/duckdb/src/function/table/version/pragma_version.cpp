b53a0e945ccaadb1d800b4153d4e50df32cdb0f0
krlmlr <krlmlr@users.noreply.github.com>
2026-05-29 07:19:29 +0000

    vendor: Update vendored sources to duckdb/duckdb@a504c24a473f758b2e7da812bd76d670bb479673
    Date: 2026-05-29 09:11:38 +0200
    
    `LEFT` Join Reordering (https://redirect.github.com/duckdb/duckdb/pull/22929)
    Bump SQLite (https://redirect.github.com/duckdb/duckdb/pull/22945)
    Test Runner: Allow for variable expansion also in "set variable", and allow for grabbing variables from any connection (https://redirect.github.com/duckdb/duckdb/pull/22943)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7350"
+#define DUCKDB_PATCH_VERSION "0-dev7460"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev7350"
+#define DUCKDB_VERSION "v1.6.0-dev7460"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "7ce0f734aa"
+#define DUCKDB_SOURCE_ID "a504c24a47"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
