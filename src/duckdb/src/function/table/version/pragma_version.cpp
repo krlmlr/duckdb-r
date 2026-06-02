93fe89ac3f2e2bd2265caea803a0aeef0aa8ada1
krlmlr <krlmlr@users.noreply.github.com>
2026-06-02 15:23:44 +0000

    vendor: Update vendored sources to duckdb/duckdb@7dcfd4b97e354832a2c1c7bc77b76e73033f7652
    Date: 2026-06-02 12:42:08 +0000
    
    Let AsyncExecutionTask always notify blocked pipeline when a task throws (https://redirect.github.com/duckdb/duckdb/pull/23001)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7696"
+#define DUCKDB_PATCH_VERSION "0-dev7700"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev7696"
+#define DUCKDB_VERSION "v1.6.0-dev7700"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "0d46559fa1"
+#define DUCKDB_SOURCE_ID "7dcfd4b97e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
