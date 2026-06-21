2de33521205070aa027dd92d5bfcaab20a8b0eb0
krlmlr <krlmlr@users.noreply.github.com>
2026-06-21 18:03:41 +0000

    vendor: Update vendored sources to duckdb/duckdb@b0f224ed774b7c29f9417803f584cc21eebef279
    Date: 2026-06-03 07:45:24 +0000
    
    Allow `SQLNULL` as a type in query results and tables (https://redirect.github.com/duckdb/duckdb/pull/23017)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7813"
+#define DUCKDB_PATCH_VERSION "0-dev7819"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev7813"
+#define DUCKDB_VERSION "v1.6.0-dev7819"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "7594f5a2d3"
+#define DUCKDB_SOURCE_ID "b0f224ed77"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
