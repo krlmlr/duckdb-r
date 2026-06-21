9a78c76d212cf33d0a3f758a6f025d41b0ebfb85
krlmlr <krlmlr@users.noreply.github.com>
2026-06-21 18:04:00 +0000

    vendor: Update vendored sources to duckdb/duckdb@d0c8617200b5b6363e839c7c505d5e2b9504a2eb
    Date: 2026-06-04 08:23:43 +0000
    
    Internal https://redirect.github.com/duckdb/duckdb/pull/9434: ENUM IN STRING_LITERAL (https://redirect.github.com/duckdb/duckdb/pull/22959)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev8011"
+#define DUCKDB_PATCH_VERSION "0-dev8019"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev8011"
+#define DUCKDB_VERSION "v1.6.0-dev8019"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d1434e02ce"
+#define DUCKDB_SOURCE_ID "d0c8617200"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
