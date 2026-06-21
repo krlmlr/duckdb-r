3da66b7188b7d48d67227299f339d3423acab5f1
krlmlr <krlmlr@users.noreply.github.com>
2026-06-21 18:03:38 +0000

    vendor: Update vendored sources to duckdb/duckdb@9ddf2e47203038eadab10735d0b5f301893c724e
    Date: 2026-06-03 08:24:00 +0200
    
    Change scope of max execution time (https://redirect.github.com/duckdb/duckdb/pull/23018)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7784"
+#define DUCKDB_PATCH_VERSION "0-dev7786"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev7784"
+#define DUCKDB_VERSION "v1.6.0-dev7786"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "28ede7f5f6"
+#define DUCKDB_SOURCE_ID "9ddf2e4720"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
