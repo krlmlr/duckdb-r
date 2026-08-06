c87f720e51e28e0c88a7546bc364dc94ddec67c6
krlmlr <krlmlr@users.noreply.github.com>
2026-08-06 15:10:20 +0000

    vendor: Update vendored sources to duckdb/duckdb@043e1894425b49984c5010f253589e5d9c5fdde4
    Date: 2026-08-05 12:13:41 +0000
    
    Add checked cast helper to `HTTPUtil` (https://redirect.github.com/duckdb/duckdb/pull/24517)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev12254"
+#define DUCKDB_PATCH_VERSION "0-dev12256"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev12254"
+#define DUCKDB_VERSION "v1.6.0-dev12256"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4ea700575c"
+#define DUCKDB_SOURCE_ID "043e189442"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
