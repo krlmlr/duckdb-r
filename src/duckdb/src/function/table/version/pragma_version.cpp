22b34364ac9b5158f6bfcafaa8a358d9a6548622
krlmlr <krlmlr@users.noreply.github.com>
2026-08-06 15:15:53 +0000

    vendor: Update vendored sources to duckdb/duckdb@89cc4bb95bf05fc1b5933699342eba37a04f3f98
    Date: 2026-08-05 15:18:01 +0000
    
    Improved tokenization for nested comments (https://redirect.github.com/duckdb/duckdb/pull/24522)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev12277"
+#define DUCKDB_PATCH_VERSION "0-dev12280"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev12277"
+#define DUCKDB_VERSION "v1.6.0-dev12280"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "8ea7d9fcae"
+#define DUCKDB_SOURCE_ID "89cc4bb95b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
