9ea86f30546309b7152ca5eccb22d16016f73d8a
krlmlr <krlmlr@users.noreply.github.com>
2026-08-06 15:16:46 +0000

    vendor: Update vendored sources to duckdb/duckdb@4e695a9b79ae6a7bf97d8ca2be3ed58637c3a53d
    Date: 2026-08-05 15:18:48 +0000
    
    Return Identifier from GetDefaultSchema/Table/TableSchema (https://redirect.github.com/duckdb/duckdb/pull/24498)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev12283"
+#define DUCKDB_PATCH_VERSION "0-dev12286"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev12283"
+#define DUCKDB_VERSION "v1.6.0-dev12286"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4707f29641"
+#define DUCKDB_SOURCE_ID "4e695a9b79"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
