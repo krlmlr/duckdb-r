16afbef904ef93bc62f2602dd5999b3a3d0816a8
Kirill Müller <kirill@cynkra.com>
2026-08-18 11:54:51 +0000

    vendor: Update vendored sources to duckdb/duckdb@61e00e80cc6c1fa640a4f3d359fc4314be25e340
    Date: 2026-08-18 08:11:01 +0000
    
    Preserve case-sensitive VARIANT fields during shredding (https://redirect.github.com/duckdb/duckdb/pull/24716)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev12864"
+#define DUCKDB_PATCH_VERSION "0-dev12869"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev12864"
+#define DUCKDB_VERSION "v1.6.0-dev12869"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a1353f264b"
+#define DUCKDB_SOURCE_ID "61e00e80cc"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
