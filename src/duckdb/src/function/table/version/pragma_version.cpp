568e5d9a2aca351a7279ae7edcc7cf8af4dfdd39
Kirill Müller <kirill@cynkra.com>
2026-08-18 11:50:32 +0000

    vendor: Update vendored sources to duckdb/duckdb@cd6b42dd5a198ced4f68fab753036d7e5df231cd
    Date: 2026-08-18 08:31:50 +0000
    
    Fix Parquet interval millisecond overflow (https://redirect.github.com/duckdb/duckdb/pull/24821)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev12869"
+#define DUCKDB_PATCH_VERSION "0-dev12872"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev12869"
+#define DUCKDB_VERSION "v1.6.0-dev12872"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "61e00e80cc"
+#define DUCKDB_SOURCE_ID "cd6b42dd5a"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
