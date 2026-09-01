31630b7af48202f614238eb42432138e25d5f5d0
Kirill Müller <kirill@cynkra.com>
2026-09-01 11:47:28 +0000

    vendor: Update vendored sources to duckdb/duckdb@a257f6cff161e87e72b1507f3d140f3273647d6c
    Date: 2026-09-01 07:43:04 +0000
    
    [Dev] Use `PhysicalResultCollector::IsStreaming` rather than just the hint if streaming is allowed (https://redirect.github.com/duckdb/duckdb/pull/25179)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev83282"
+#define DUCKDB_PATCH_VERSION "0-dev83284"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 2
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v2.0.0-dev83282"
+#define DUCKDB_VERSION "v2.0.0-dev83284"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1b3c92a188"
+#define DUCKDB_SOURCE_ID "a257f6cff1"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
