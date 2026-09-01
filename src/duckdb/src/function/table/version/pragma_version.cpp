c81157414ac37efc5ff5d9147fea9811fe584d63
Kirill Müller <kirill@cynkra.com>
2026-09-01 07:46:25 +0000

    vendor: Update vendored sources to duckdb/duckdb@48ef058edf78fbb4bfd49a6287589a1b3efd54ab
    Date: 2026-08-31 12:54:37 +0000
    
    Propagate comparison for INTERVAL type (https://redirect.github.com/duckdb/duckdb/pull/25090)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev13962"
+#define DUCKDB_PATCH_VERSION "0-dev13964"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev13962"
+#define DUCKDB_VERSION "v1.6.0-dev13964"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4015290d75"
+#define DUCKDB_SOURCE_ID "48ef058edf"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
