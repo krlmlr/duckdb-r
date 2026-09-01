147dbc0dc6960e4e191bfaa190afd572db93a819
Kirill Müller <kirill@cynkra.com>
2026-09-01 08:11:34 +0000

    vendor: Update vendored sources to duckdb/duckdb@197a59970b8ea5fcbf261aecfe68aed36e2da002
    Date: 2026-09-01 07:08:13 +0000
    
    Reject `CREATE TEMPORARY INDEX` / `TRIGGER`, inherit trigger temporariness (https://redirect.github.com/duckdb/duckdb/pull/25119)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev13999"
+#define DUCKDB_PATCH_VERSION "0-dev14001"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev13999"
+#define DUCKDB_VERSION "v1.6.0-dev14001"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "84e2f64b8e"
+#define DUCKDB_SOURCE_ID "197a59970b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
