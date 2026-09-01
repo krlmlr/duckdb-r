c8862a0e4b5ea82f284e758f53e39801976931bb
Kirill Müller <kirill@cynkra.com>
2026-09-01 11:47:00 +0000

    vendor: Update vendored sources to duckdb/duckdb@84742e9260cda6c2dce7ba222617171312e482e0
    Date: 2026-09-01 07:34:19 +0000
    
    Fix: Do not produce invalid UTF8 from printf's %c conversion (https://redirect.github.com/duckdb/duckdb/pull/25189)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev83277"
+#define DUCKDB_PATCH_VERSION "0-dev83279"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 2
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v2.0.0-dev83277"
+#define DUCKDB_VERSION "v2.0.0-dev83279"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b82f19b24e"
+#define DUCKDB_SOURCE_ID "84742e9260"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
