0487f2f7f43f139b40601f9c45a096f8673c1850
Kirill Müller <kirill@cynkra.com>
2026-09-01 07:46:58 +0000

    vendor: Update vendored sources to duckdb/duckdb@605cdf985c313a0e0480e133fdc4da5a83342c53
    Date: 2026-08-31 16:20:31 +0000
    
    Move SigV4 signing logic from httpfs into core (https://redirect.github.com/duckdb/duckdb/pull/25141)
    Use namespace runners and reduce OSX CI runs (https://redirect.github.com/duckdb/duckdb/pull/25182)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev13969"
+#define DUCKDB_PATCH_VERSION "0-dev13974"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 6
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev13969"
+#define DUCKDB_VERSION "v1.6.0-dev13974"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "492b0a30fe"
+#define DUCKDB_SOURCE_ID "605cdf985c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
